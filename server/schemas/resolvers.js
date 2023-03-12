2a2e81ed1f3687bd2645ab94a76d9477b40b9e99
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-12 13:56:31 -0600

    Login mutation resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,8 +17,23 @@ const resolvers = {
     addUser: async (parent, args) => {
       // create new user, sign token, return token and user data
     },
-    login: async (parent, args) => {
+    login: async (parent, { email, password }) => {
       // find user, check password, sign token, return token and user data
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
     },
     saveBook: async (parent, args, context) => {
       // protect route, find current user, update their savedBooks array, return user data
